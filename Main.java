b2a9e547c13e84c874270b5059e3ac9b2f6d96aa
shekhar-anshu <shekhar.anshu@outlook.com>
2025-01-02 09:11:37 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("\n\rThis is java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+}
